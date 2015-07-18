a586765b9901fee5307d1693348af3aa9a2f027d
Mahmoud Saada <mahmoud@targetcw.com>
2015-07-18 09:09:38 -0700

    improved keys file structure

--- keys/digitalocean/digitalocean.zsh
+++ keys/digitalocean/digitalocean.zsh
@@ -1,6 +1,1 @@
-if [ ! -f $HOME/.keys/digitalocean/secret ]; then
-    export DIGITAL_OCEAN_ACCESS_KEY=''
-    echo "No DigitalOcean key found..."
-  else
-    export DIGITAL_OCEAN_ACCESS_KEY=$(cat $HOME/.keys/digitalocean/secret)
-fi
+source $HOME/.keys/digitalocean

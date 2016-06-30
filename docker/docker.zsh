4d86a110ea015a708ef212d2e665e9434bf03bbe
Mahmoud Saada <mahmoud@targetcw.com>
2016-06-30 10:35:41 -0400

    add more docker cleanup commands

--- docker/docker.zsh
+++ docker/docker.zsh
@@ -1,5 +1,5 @@
 # setup the dev docker machine if it's running
-docker-machine ls | grep dev | grep -i running && eval "$(docker-machine env dev)"
+#docker-machine ls | grep dev | grep -i running && eval "$(docker-machine env dev)"
 
 # source: @jfrazelle's dotfiles
 hollywood(){
@@ -18,21 +18,25 @@ htop(){
 
 # remove exited containers and dangling images. greatly frees up disk space
 docker_clean() {
-    echo "cleaning up exited containers and dangling images..."
-    # suppress errors
-    docker rm -v $(docker ps --filter status=exited -q 2>/dev/null) 2>/dev/null || true
+  echo "cleaning up exited containers and dangling images..."
+  # suppress errors
+  docker rm -v $(docker ps --filter status=exited -q 2>/dev/null) 2>/dev/null || true
 	docker rmi $(docker images --filter dangling=true -q 2>/dev/null) 2>/dev/null || true
-    docker rmi $(docker images | grep '^<none>' | awk '{print $3}' 2>/dev/null) 2>/dev/null || true
+  docker rmi $(docker images | grep '^<none>' | awk '{print $3}' 2>/dev/null) 2>/dev/null || true
 }
 
 # remove ALL containers (including running) and images
 docker_wipe() {
-    docker rm -f $(docker ps -aq) 2>/dev/null || true
-    docker rmi -f $(docker images -aq) 2>/dev/null || true
+  docker rm -f $(docker ps -aq) 2>/dev/null || true
+  docker rmi -f $(docker images -aq) 2>/dev/null || true
 }
 
 # kill all containers
 docker_kill_all() {
   echo "killing all containers..."
   docker kill $(docker ps -q)
+}
+
+docker_gc() {
+	docker run --rm -v /var/run/docker.sock:/var/run/docker.sock -v /etc:/etc spotify/docker-gc
 }
